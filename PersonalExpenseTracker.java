0778392e785812a0cec58960c304010641ca38d2
Diya Panchal <diya.pce21@sot.pdpu.ac.in>
2024-12-14 00:08:02 +0530

    Expense Class declaration

--- PersonalExpenseTracker.java
+++ PersonalExpenseTracker.java
@@ -1,3 +1,37 @@
+import java.io.*;
+import java.util.*;
+
 public class PersonalExpenseTracker {
-    
+    private static List<Expense> expenses = new ArrayList<>();
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Expense {
+    private String date;
+    private String category;
+    private double amount;
+    private String description;
+
+    public Expense(String date, String category, double amount, String description) {
+        this.date = date;
+        this.category = category;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String toCsv() {
+        return date + "," + category + "," + amount + "," + description;
+    }
+
+    @Override
+    public String toString() {
+        return "Date: " + date + ", Category: " + category + ", Amount: " + amount + ", Description: " + description;
+    }
 }
